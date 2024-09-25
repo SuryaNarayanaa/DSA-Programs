a5c6f3a600ae0e3ff3ce8f9341837975236b59d4
Surya Narayanaa N T <81570389+SuryaNarayanaa@users.noreply.github.com>
2024-09-25 09:58:54 +0530

    Added practice problems

--- Linked List/LinkedList-ADT.c
+++ Linked List/LinkedList-ADT.c
@@ -19,8 +19,8 @@ Node* createNode(int data)
 
 
     return node;
-    
-} 
+
+}
 
 
 int findLength (Node* head)
@@ -67,7 +67,7 @@ Node* traverseToPosition(Node* head, int index)
     while(count<index)
     {
             curr = curr->next;
-            count++;      
+            count++;
     }
 
     return curr;
@@ -136,48 +136,174 @@ Node* deleteAtEnd(Node* head)
 
 Node* deleteAtPosition(Node* head, int position)
 {   if (head ==NULL) return NULL;
-    if(head->next== NULL  && position ==1) 
+    if(head->next== NULL  && position ==1)
     {
-        head =NULL;  
+        head =NULL;
         return head;
     }
     else if (head->next == NULL)
     {return NULL;}
 
-  
+
     Node* previousNode = traverseToPosition(head, position);
     Node* NodeToDelete = previousNode->next;
     previousNode->next = NodeToDelete->next;
-    
+
 
 
 
     return head;
 
 
-    
+
 }
+
+
+
+Node* reverseLinkedList(Node* head)
+{
+    Node* prev  = NULL;
+    Node* curr = head;
+    Node* next ;
+
+ while(curr != NULL)
+ {
+     next  = curr->next;
+     curr->next = prev;
+
+     prev = curr;
+     curr = next;
+
+ }
+
+ return prev;
+}
+Node* removeDuplicatesInSortedLL(Node* head)
+{
+    Node* curr= head;
+    int element =1;
+    while(curr->next!=NULL)
+    {
+        if(curr->data == curr->next->data)
+        {
+            deleteAtPosition(head , element);
+
+        }
+        curr = curr->next;
+        element++;
+    }
+    return head;
+
+
+}
+
+Node* merge2LLIntoALternateLL(Node* head1, Node* head2)
+{
+    Node* mergedLL= NULL ;
+    Node*  curr1=head1;
+    Node*  curr2 =head2 ;
+    Node* mergedHead =NULL;
+    int curr1Data, curr2Data;
+    if (curr1 == NULL) return curr2;
+    if (curr2== NULL ) return curr1;
+    while(curr1 && curr2)
+    {
+        curr1Data = curr1->data;
+        curr2Data = curr2->data;
+        if(mergedLL != NULL)
+        {
+
+            mergedLL = insertAtEnd(mergedLL , curr1Data);
+            mergedLL =insertAtEnd(mergedLL , curr2Data);
+        }
+        else
+        {
+             mergedHead = insertAtEnd(mergedLL , curr1Data);
+            mergedLL =insertAtEnd(mergedHead , curr2Data);
+
+        }
+
+
+        curr1 = curr1->next;
+        curr2= curr2->next;
+
+    }
+    if(curr1)
+    { mergedLL->next = curr1;
+    }
+    else if(curr2)
+    {
+
+        mergedLL->next = curr2;
+    }
+
+    return mergedHead;
+}
+
+
+Node* sortTernary(Node* head)
+{
+    Node* curr = head;
+    int cnt[3] = {0},  pos ,count , ind=0;
+    while(curr!=NULL)
+    {
+        cnt[curr->data]++;
+        curr = curr->next;
+    }
+    curr = head;
+    while(curr!= NULL)
+    {
+        if(cnt[ind]==0 )
+        {
+         ind++;
+        }
+        else{
+            curr->data = ind;
+            cnt[ind]--;
+            curr = curr->next;
+        }
+    }
+    return head;
+    }
+
+
 int main()
 {
 int len, elem, position;
-printf("Enter the length of the Linked List : ");
+printf("Enter the length of the Linked List 1 : ");
 scanf("%d", &len);
 
 
 
-Node* head= NULL;
+Node* head1= NULL ;
+Node* head;
 
 
 for (int i=1 ;i<=len; i++)
 {
     printf("Enter the Element %d", i);
     scanf("%d", &elem);
-    head = insertAtEnd(head, elem);
+    head1 = insertAtEnd(head1, elem);
+
+}
+printf("Enter the length of the Linked List 2 : ");
+scanf("%d", &len);
+
+
+/*
+
+Node* head2= NULL;
+
+
+for (int i=1 ;i<=len; i++)
+{
+    printf("Enter the Element %d", i);
+    scanf("%d", &elem);
+    head2 = insertAtEnd(head2, elem);
 
 }
 
-
-
+*/
     // printf("Enter the Element to add at beginning : ");
     // scanf("%d", &elem);
     // head = inserAtBeginning(head, elem);
@@ -193,19 +319,22 @@ for (int i=1 ;i<=len; i++)
 
     // head  = deleteAtEnd(head);
 
-    printf("Enter the position to delete : ");
-    scanf("%d", &position);
+    //printf("Enter the position to delete : ");
+    //scanf("%d", &position);
 
-    head = deleteAtPosition(head, position);
+    //head = deleteAtPosition(head, position);
 
-    printf("\nLenght of the linked list: %d\n\n\n", findLength(head));
-
-
+    //printf("\nLenght of the linked list: %d\n\n\n", findLength(head));
 
 
-displayNode(head);
+    //head =  removeDuplicatesInSortedLL(head);
+    //head = merge2LLIntoALternateLL(head1 ,head2);
+
+    head = sortTernary(head1);
 
 
 
+
+displayNode(head1);
 
 }
